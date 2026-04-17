f9ac9705cfa5586cc2339552ec8547465c7a3ff8
Cristiano Camacho <originalvgo@gmail.com>
2026-04-16 22:05:15 -0300

    feat: add hello world demonstration (Step 1)

---
+++ 1-hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * C++ REVISION #1: Hello World
+ * The starting point of any C++ program.
+ */
+
+int main() {
+    // cout is used to print text to the console
+    // endl inserts a new line and flushes the buffer
+    cout << "Hello, C++ Revision!" << endl;
+    
+    return 0;
+}
