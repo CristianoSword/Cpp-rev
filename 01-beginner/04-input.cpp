c22fb4cfbb23d4d299bb01a408dbf8ac736ee34e
Cristiano Camacho <originalvgo@gmail.com>
2026-04-21 23:41:14 -0300

    refactor(04-input): remove 'using namespace std;' and qualify std names

--- 01-beginner/04-input.cpp
+++ 01-beginner/04-input.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 /**
  * C++ REVISION #4: User Input
- * Using cin to read data from the standard input.
+ * Using std::cin to read data from the standard input.
  */
 
 int main() {
-    string fullName;
+    std::string fullName;
     int age;
 
-    cout << "Enter your age: ";
-    cin >> age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
 
-    // We can use cin.ignore() to clear the newline character 
-    // before reading a string with getline
-    cin.ignore();
+    // We can use std::cin.ignore() to clear the newline character
+    // before reading a string with std::getline
+    std::cin.ignore();
 
-    cout << "Enter your full name: ";
-    getline(cin, fullName);
+    std::cout << "Enter your full name: ";
+    std::getline(std::cin, fullName);
 
-    cout << "Hello " << fullName << "! You are " << age << " years old." << endl;
+    std::cout << "Hello " << fullName << "! You are " << age << " years old." << std::endl;
 
     return 0;
 }
