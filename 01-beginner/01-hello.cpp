d487dacf332e04e8d58343139c70f429e5e20444
Cristiano Camacho <originalvgo@gmail.com>
2026-04-21 23:41:14 -0300

    refactor(01-hello): remove 'using namespace std;' and qualify std names

--- 01-beginner/01-hello.cpp
+++ 01-beginner/01-hello.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-using namespace std;
-
 /**
  * C++ REVISION #1: Hello World
  * The starting point of any C++ program.
  */
 
 int main() {
-    // cout is used to print text to the console
-    // endl inserts a new line and flushes the buffer
-    cout << "Hello, C++ Revision!" << endl;
-    
+    // std::cout is used to print text to the console
+    // std::endl inserts a new line and flushes the buffer
+    std::cout << "Hello, C++ Revision!" << std::endl;
+
     return 0;
 }
