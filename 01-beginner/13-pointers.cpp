b79d1354e50dcfdaa51e4829fb00f63884755c4c
Cristiano Camacho <originalvgo@gmail.com>
2026-04-21 23:41:31 -0300

    refactor(13-pointers): remove 'using namespace std;' and qualify std names

--- 01-beginner/13-pointers.cpp
+++ 01-beginner/13-pointers.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 /**
  * C++ REVISION #13: Pointers and References
  * Understanding memory addresses and direct memory access.
@@ -9,23 +7,23 @@ using namespace std;
 
 int main() {
     int value = 42;
-    
+
     // Reference: an alias for an existing variable
-    int &ref = value; 
+    int &ref = value;
     ref = 100; // Changes 'value' to 100
-    
-    cout << "Value via reference: " << ref << endl;
-    cout << "Original value: " << value << endl;
+
+    std::cout << "Value via reference: " << ref << std::endl;
+    std::cout << "Original value: " << value << std::endl;
 
     // Pointer: a variable that stores a memory address
     int *ptr = &value; // & is the address-of operator
 
-    cout << "\nMemory Address: " << ptr << endl;
-    cout << "Value via pointer (dereferencing): " << *ptr << endl;
+    std::cout << "\nMemory Address: " << ptr << std::endl;
+    std::cout << "Value via pointer (dereferencing): " << *ptr << std::endl;
 
     // Modifying value via pointer
     *ptr = 500;
-    cout << "New value: " << value << endl;
+    std::cout << "New value: " << value << std::endl;
 
     return 0;
 }
